ff4fba530e66c104745ad25364e438529388cef8
Muzammil Shah <207419428+codephile-muzammil@users.noreply.github.com>
2025-09-29 12:22:44 +0530

    Added string iteration template code

---
+++ strings/iterate_string_template.c
@@ -0,0 +1,18 @@
+/*
+Author: Muzammil
+Purpose: Template to iterate over a string character by character
+Usage: Replace the commented line with your logic
+*/
+#include <stdio.h>
+
+int main() 
+{   int i;
+    char var_name[] = "example";
+    i = 0;
+    while (var_name[i] != '\0')
+    {
+        // Enter your logic here
+        i++;
+    }
+    return 0;
+}
